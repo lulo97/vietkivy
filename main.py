f5ebfe9b46fed0609ec23f2e6d1de047e2bd198c
lulo97 <luongpysl2@gmail.com>
2024-08-05 10:42:41 +0700

    Add new files from folder

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from kivy.app import App
 from kivy.uix.label import Label
 
-class HelloWorldApp(App):
+class HelloWorldApp(App): 
     def build(self):
-        return Label(text='Hello World')
+        return Label(text='Hello World123')
 
 if __name__ == '__main__':
     HelloWorldApp().run()
